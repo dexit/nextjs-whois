eb0886d5350383c6b8609476ab811d43e538c8eb
Caliph Dev <admin@caliph.my.id>
2022-07-07 06:57:18 +0700

    Update index.js

--- src/components/WhoisResultData/index.js
+++ src/components/WhoisResultData/index.js
@@ -52,7 +52,7 @@ const WhoisResultData = ({ result }) => {
                           <tr>
                             <th scope="row">{t("register-date")}</th>
                             <td>
-                              {moment(new Date(data.creationDate * 1000)).format("MM/DD/YYYY")}
+                              {moment(new Date(data.creationDate)).format("MM/DD/YYYY")}
                             </td>
                           </tr>
                         )}
@@ -68,7 +68,7 @@ const WhoisResultData = ({ result }) => {
                           <tr>
                             <th scope="row">{t("expiry-date")}</th>
                             <td>
-                              {moment(new Date(data.expirationDate * 1000)).format("MM/DD/YYYY")}
+                              {moment(new Date(data.expirationDate)).format("MM/DD/YYYY")}
                             </td>
                           </tr>
                         )}
@@ -76,7 +76,7 @@ const WhoisResultData = ({ result }) => {
                           <tr>
                             <th scope="row">{t("remaining-time")}</th>
                             <td>
-                              {moment(new Date(data.expirationDate * 1000)).diff(
+                              {moment(new Date(data.expirationDate)).diff(
                                 moment(),
                                 "days"
                               )}
